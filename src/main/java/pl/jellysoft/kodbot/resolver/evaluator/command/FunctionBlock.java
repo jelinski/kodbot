4f3b3bf2a271f2542b9482e71dca478b8167adea
Krzysztof Jeliński <krzysztof.jelinski@gmail.com>
2020-05-04 22:35:33 +0200

    Fixed issue with equals and hashcode

--- src/main/java/pl/jellysoft/kodbot/resolver/evaluator/command/FunctionBlock.java
+++ src/main/java/pl/jellysoft/kodbot/resolver/evaluator/command/FunctionBlock.java
@@ -1,10 +1,12 @@
 package pl.jellysoft.kodbot.resolver.evaluator.command;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode(callSuper = false)
 public class FunctionBlock extends Block {
 
     public static final String KEYWORD = "function";
@@ -12,10 +14,5 @@ public class FunctionBlock extends Block {
     public static final String COMMANDS_KEYWORD = "commands";
 
     private String name;
-
-    @Override
-    public boolean equals(Object obj) { // TODO make use of Lombok
-        return obj instanceof FunctionBlock && ((FunctionBlock) obj).name.equals(this.name);
-    }
 
 }
