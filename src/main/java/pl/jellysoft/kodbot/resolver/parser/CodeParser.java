eae2345d6657277d2c7e484ad463be3542e4f501
Krzysztof Jeliński <krzysztof.jelinski@gmail.com>
2020-05-20 22:00:57 +0200

    CodeParses refactorings - reduced boilerplate and removed system.out usages

--- src/main/java/pl/jellysoft/kodbot/resolver/parser/CodeParser.java
+++ src/main/java/pl/jellysoft/kodbot/resolver/parser/CodeParser.java
@@ -2,28 +2,25 @@ package pl.jellysoft.kodbot.resolver.parser;
 
 import org.antlr.v4.runtime.ANTLRErrorListener;
 import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.BaseErrorListener;
 import org.antlr.v4.runtime.CommonToken;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.Parser;
 import org.antlr.v4.runtime.RecognitionException;
 import org.antlr.v4.runtime.Recognizer;
 import org.antlr.v4.runtime.Vocabulary;
 import org.antlr.v4.runtime.VocabularyImpl;
-import org.antlr.v4.runtime.atn.ATNConfigSet;
-import org.antlr.v4.runtime.dfa.DFA;
 import pl.jellysoft.kodbot.resolver.parser.antlr.KodbotBaseListener;
 import pl.jellysoft.kodbot.resolver.parser.antlr.KodbotLexer;
 import pl.jellysoft.kodbot.resolver.parser.antlr.KodbotParser;
 
 import java.util.ArrayList;
-import java.util.BitSet;
 import java.util.List;
 
 public class CodeParser extends pl.jellysoft.kodbot.resolver.parser.Parser {
 
     private final List<String> errors = new ArrayList<>();
 
-    private final ANTLRErrorListener parserErrorListener = new ANTLRErrorListener() {
+    private final ANTLRErrorListener parserErrorListener = new BaseErrorListener() {
         @Override
         public void syntaxError(Recognizer<?, ?> arg0, Object arg1, int arg2, int arg3, String arg4, RecognitionException arg5) {
             CommonToken currentToken = (CommonToken) arg1;
@@ -31,46 +28,15 @@ public class CodeParser extends pl.jellysoft.kodbot.resolver.parser.Parser {
             Vocabulary vocabulary = VocabularyImpl.fromTokenNames(kodbotParser.getTokenNames());
             String expectedTokens = kodbotParser.getExpectedTokens().toString(vocabulary);
             String error = "Blad w linii:" + arg2 + " znak:" + arg3 + " otrzymano: " + currentToken.getText() + " spodziewano sie:" + expectedTokens;
-            System.out.println(error);
             errors.add(error);
-        }
-
-        @Override
-        public void reportContextSensitivity(Parser arg0, DFA arg1, int arg2, int arg3, int arg4, ATNConfigSet arg5) {
-            System.out.println("parserErrorListener reportContextSensitivity");
-        }
-
-        @Override
-        public void reportAttemptingFullContext(Parser arg0, DFA arg1, int arg2, int arg3, BitSet arg4, ATNConfigSet arg5) {
-            System.out.println("parserErrorListener reportAttemptingFullContext");
-        }
-
-        @Override
-        public void reportAmbiguity(Parser arg0, DFA arg1, int arg2, int arg3, boolean arg4, BitSet arg5, ATNConfigSet arg6) {
-            System.out.println("parserErrorListener reportAmbiguity");
         }
     };
 
-    private final ANTLRErrorListener lexerErrorListener = new ANTLRErrorListener() {
+    private final ANTLRErrorListener lexerErrorListener = new BaseErrorListener() {
         @Override
         public void syntaxError(Recognizer<?, ?> arg0, Object arg1, int arg2, int arg3, String arg4, RecognitionException arg5) {
             String error = "Blad skladni w linii: " + arg2 + " znak: " + arg3;
             errors.add(error);
-        }
-
-        @Override
-        public void reportContextSensitivity(Parser arg0, DFA arg1, int arg2, int arg3, int arg4, ATNConfigSet arg5) {
-            System.out.println("lexerErrorListener reportContextSensitivity");
-        }
-
-        @Override
-        public void reportAttemptingFullContext(Parser arg0, DFA arg1, int arg2, int arg3, BitSet arg4, ATNConfigSet arg5) {
-            System.out.println("lexerErrorListener reportAttemptingFullContext");
-        }
-
-        @Override
-        public void reportAmbiguity(Parser arg0, DFA arg1, int arg2, int arg3, boolean arg4, BitSet arg5, ATNConfigSet arg6) {
-            System.out.println("lexerErrorListener reportAmbiguity");
         }
     };
 
@@ -84,12 +50,7 @@ public class CodeParser extends pl.jellysoft.kodbot.resolver.parser.Parser {
         KodbotBaseListener listener = new KodbotBaseListener();
         parser.addParseListener(listener);
         parser.addErrorListener(parserErrorListener);
-
-        try {
-            parser.start();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        parser.start();
 
         if (errors.size() > 0) {
             throw new ParserException(errors.toString());
