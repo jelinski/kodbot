423ba82f5a8fda25b0abfce9b13861d5990c672f
Krzysztof Jeliński <krzysztof.jelinski@gmail.com>
2020-05-28 23:01:37 +0200

    Improvements to VariableContainer

--- src/main/java/pl/jellysoft/kodbot/resolver/evaluator/VariableContainer.java
+++ src/main/java/pl/jellysoft/kodbot/resolver/evaluator/VariableContainer.java
@@ -1,18 +1,18 @@
 package pl.jellysoft.kodbot.resolver.evaluator;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class VariableContainer {
 
     private static final int VARIABLE_MAX_VALUE = 20;
-
-    private final ArrayList<Pair> variables = new ArrayList<>();
+    private final Map<String, Integer> variables = new HashMap<>();
 
     public void setVariable(String name, String value) throws EvaluatorException {
         validate(name);
 
         Integer rightValue;
-        if (isDigit(value)) {
+        if (isNumeric(value)) {
             rightValue = Integer.valueOf(value);
             if (rightValue > VARIABLE_MAX_VALUE) {
                 throw new EvaluatorException("Za duza wartosc dla zmiennej: " + name + ". Maksymalna wartość to: " + VARIABLE_MAX_VALUE);
@@ -21,72 +21,28 @@ public class VariableContainer {
             rightValue = getVariable(value);
         }
 
-        for (Pair p : variables) {
-            if (p.getLeft().equals(name)) {
-                p.setRight(rightValue);
-                return;
-            }
-        }
-
-        variables.add(new Pair(name, rightValue));
+        variables.put(name, rightValue);
     }
 
-    public Integer getVariable(String name) throws EvaluatorException {
-        validate(name);
-        for (Pair p : variables) {
-            if (p.getLeft().equals(name)) {
-                return p.getRight();
-            }
-        }
-        return 0;
+    private Integer getVariable(String name) {
+        return variables.getOrDefault(name, 0);
     }
 
     public Integer getValue(String nameOrValue) {
-        if (isDigit(nameOrValue)) {
+        if (isNumeric(nameOrValue)) {
             return Integer.valueOf(nameOrValue);
         } else {
-            for (Pair p : variables) {
-                if (p.getLeft().equals(nameOrValue)) {
-                    return p.getRight();
-                }
-            }
-            return 0;
+            return getVariable(nameOrValue);
         }
     }
 
-    private boolean isDigit(String val) {
+    private boolean isNumeric(String val) {
         return val.matches("[0-9]+");
     }
 
     private void validate(String name) throws EvaluatorException {
-        if (isDigit(name)) {
+        if (isNumeric(name)) {
             throw new EvaluatorException("Cyfry po lewej stronie przypisania");
-        }
-    }
-
-    private static class Pair {
-        private String left;
-        private Integer right;
-
-        public Pair(String left, Integer right) {
-            setLeft(left);
-            setRight(right);
-        }
-
-        public String getLeft() {
-            return left;
-        }
-
-        public void setLeft(String left) {
-            this.left = left;
-        }
-
-        public Integer getRight() {
-            return right;
-        }
-
-        public void setRight(Integer right) {
-            this.right = right;
         }
     }
 
